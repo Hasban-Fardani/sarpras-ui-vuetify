558e76e4e3a8c383358f057387658411282de130
Hasban-Fardani <hasbanfardani77@gmail.com>
2024-05-27 15:16:57 +0700

    feat: add simple profile

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -4,10 +4,37 @@ import UserLayout from '@/layouts/UserLayout.vue';
 import { useUserStore } from '@/stores/user';
 
 const user = useUserStore()
-const Layout = user.data.role === 'admin' ? AdminLayout : UserLayout
+const CLayout = user.data.role === 'admin' ? AdminLayout : UserLayout
 </script>
 <template>
-    <Layout>
-        todo: profile
-    </Layout>
+    <c-layout>
+        <v-card class="mt-4">
+            <v-card-text>
+                <div class="d-flex justify-center align-center">
+                    <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="user avatar" width="200px">
+                </div>
+                <h3 class="text-center mt-2">Profile</h3>
+
+                <v-row class="mt-3">
+                    <v-col>
+                        <v-text-field label="Name" v-model="user.data.name"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field label="Password" type="password" value="*****" disabled></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col>
+                        <v-text-field label="Role" disabled></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field label="Unit" disabled></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-btn color="primary">
+                    Save
+                </v-btn>
+            </v-card-text>
+        </v-card>
+    </c-layout>
 </template>
--- src/views/user/RequestView.vue
+++ src/views/user/RequestView.vue
@@ -1,20 +1,49 @@
 <script setup lang="ts">
 import UserLayout from '@/layouts/UserLayout.vue';
+import { ref } from 'vue';
+
+const step = ref(1)
+const next = () => {
+    if (step.value < 3) {
+        step.value++
+    } else {
+        console.log('lebihh');
+    }
+}
+const prev = () => {
+    if (step.value > 1) {
+        step.value--
+    }
+}
+const disable = ref(false)
 </script>
 <template>
     <UserLayout>
         <h2 class="my-3">Form <span class="text-primary">Permintaan</span></h2>
-        <v-stepper :items="['Step 1', 'Step 2', 'Step 3']">
+        <v-stepper :items="['1. Isi Data', '2. Tambahkan Barang', '3. Review']" v-model="step">
             <template v-slot:item.1>
-                <v-card title="Step One" flat>...</v-card>
+                <v-card title="Masukkan Data" flat>
+                    <v-card-text>
+                        <v-text-field label="Name"></v-text-field>
+                        <v-text-field label="Message"></v-text-field>
+                    </v-card-text>
+                </v-card>
             </template>
 
             <template v-slot:item.2>
-                <v-card title="Step Two" flat>...</v-card>
+                <v-card title="Tambahkan Barang" flat>
+                    <v-card-text></v-card-text>
+                </v-card>
             </template>
 
             <template v-slot:item.3>
-                <v-card title="Step Three" flat>...</v-card>
+                <v-card title="Review" flat>
+                    <v-card-text></v-card-text>
+                </v-card>
+            </template>
+
+            <template v-slot:actions>
+                <v-stepper-actions :disabled="disable" @click:next="next" @click:prev="prev"></v-stepper-actions>
             </template>
         </v-stepper>
     </UserLayout>
