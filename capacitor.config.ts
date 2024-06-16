6dcdba1042cb18212f535b59d96e9d748c4c7504
Hasban-Fardani <hasbanfardani77@gmail.com>
2024-06-16 11:57:30 +0700

    ref: webdir capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.sarpras.app',
   appName: 'sarpras-sebelas',
-  webDir: 'public'
+  webDir: 'dist'
 };
 
 export default config;
